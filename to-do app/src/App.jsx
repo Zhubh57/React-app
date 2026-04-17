de6e8ec014aafecae28d9295780176f2f31e307a
Chirag H M <churaghm06@gmail.com>
2026-04-17 14:29:00 +0530

    commited

---
+++ to-do app/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header"
+import Todoinput from "./components/to-do-inputs"
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Todoinput />
+    </>
+  )
+}
+
+export default App
---
+++ to-do app/src/components/to-do-inputs.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+function Todoinput({ addTodo }) {
+  const [todo, setTodo] = useState("")
+
+  const handleAdd = () => {
+    if (todo.trim() !== "") {
+      addTodo(todo)
+      setTodo("")
+    }
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter your todo"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={handleAdd}>Add</button>
+    </div>
+  )
+}
+
+export default Todoinput
